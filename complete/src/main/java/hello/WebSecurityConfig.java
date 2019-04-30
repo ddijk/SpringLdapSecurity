0bcf9035ec96c139c709e033bfdcd65be7581dc8
ddijk <dick@dijkrosoft.nl>
2019-04-30 15:16:26 +0200

    Works with bind auth

--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,29 +8,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
+@ConfigurationProperties(prefix = "security.ldap")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.anyRequest().fullyAuthenticated()
-				.and()
-			.formLogin();
-	}
+    String url;
 
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.ldapAuthentication()
-				.userDnPatterns("uid={0},ou=people")
-				.groupSearchBase("ou=groups")
-				.contextSource()
-					.url("ldap://localhost:8389/dc=springframework,dc=org")
-					.and()
-				.passwordCompare()
-					.passwordEncoder(new LdapShaPasswordEncoder())
-					.passwordAttribute("userPassword");
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
+    }
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        System.out.println("url = " + url);
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+//                .groupSearchBase("ou=groups")
+                .contextSource()
+//                .root("dc=tudelft,dc=example,dc=org")
+                .managerDn("cn=admin,dc=example,dc=org")
+                .managerPassword("admin")
+                .url(url)
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
